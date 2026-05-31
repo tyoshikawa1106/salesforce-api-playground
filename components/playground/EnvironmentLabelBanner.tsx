e7b25b995d9ca62b90a65a3c55561c0d86a73577
Taiki Yoshikawa <tyoshikawa1106@icloud.com>
2026-05-31 14:20:19 +0900

    環境ラベルをSLDSテクスチャ表示に変更

--- components/playground/EnvironmentLabelBanner.tsx
+++ components/playground/EnvironmentLabelBanner.tsx
@@ -6,9 +6,9 @@ export function EnvironmentLabelBanner({ environmentLabel }: { environmentLabel:
     }
 
     return (
-        <div className="slds-notify slds-notify_alert playground-environment-label" role="status">
+        <div className="slds-notify slds-notify_alert slds-theme_inverse slds-theme_alert-texture playground-environment-label" role="status">
             <span className="slds-assistive-text">環境ラベル</span>
-            <span className="slds-badge playground-environment-label__badge">
+            <span className="slds-text-title_bold playground-environment-label__text">
                 {environmentLabel.label}
             </span>
         </div>
